3f48272eff77f53218e356f27049b52622021709
Matthias <littlecheetah@ct4s.de>
2021-11-03 21:40:41 +0100

    force white color for logotext

--- frontend/src/views/menu.vue
+++ frontend/src/views/menu.vue
@@ -20,7 +20,7 @@ a {
 <template>
     <v-app-bar app class="transparent">
         <router-link :to="{ name: 'index' }">
-            <v-toolbar-title> Cheetahvault </v-toolbar-title>
+            <v-toolbar-title class="white--text"> Cheetahvault </v-toolbar-title>
         </router-link>
 
         <template v-if="!titleOnly">
